4e5ac53c8c6dd969037adc088b84e8bad7c04a2f
chrishillman <chris.hillman@gmail.com>
2014-06-05 11:05:17 -0400

    Update dnsreader.py
    Took out some debugging procedures.

--- dnsreader.py
+++ dnsreader.py
@@ -6,17 +6,11 @@ c.execute("CREATE TABLE dnsDB (datedns TEXT, timedns TEXT, ampm TEXT, protocol T
 readlist = list()
 with open("dnstextsample.txt") as file:
 	for line in file:
-		line = line.replace("\n", "")
-		line = line.replace("\r", "")
+		line = line.rstrip()
 		linearr = line.split(" ")
 		for i in range(0, linearr.count('')):
 			linearr.remove('')
-		for i in range(0, len(linearr)):
-			print str(i) + "  " + linearr[i]
-		print"      "
 		readlist.append((linearr[0],linearr[1],linearr[2],linearr[6],linearr[7],linearr[8],linearr[-1]))
 c.executemany("INSERT INTO dnsDB VALUES (?,?,?,?,?,?,?)", readlist)
-for row in c.execute("SELECT * FROM dnsDB"):
-	print row
 conn.commit()
 conn.close()
